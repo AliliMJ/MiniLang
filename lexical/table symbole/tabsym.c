7d8c6b0ca7139729394f7da25e4d9cbd3bc66dc8
abdelkrim99 <kmioalg03@gmail.com>
2022-04-07 04:07:37 +0100

    v

--- lexical/table symbole/tabsym.c
+++ lexical/table symbole/tabsym.c
@@ -36,6 +36,22 @@ unsigned long hash_func(char* M)
     return (code % 100) ;
 }
 
+void strtohigher(char *s) // cette fonction a meme role de tosuper mais elle converte un chaine
+{
+    int i;
+    char *p;
+
+    p = allouerstr();
+    for (i = 0; i < strlen(s); i++)
+    {
+        p[i] = toupper(s[i]);
+    }
+    p[i] = '\0';
+    s[0] = '\0';
+    strcat(s, p);
+    free(p);
+}
+
 ptrTAB initialiter(long n) // fonction pour allouer et initialiser le tableau de hachage
 {
     long i;
@@ -76,3 +92,62 @@ void affiche(ptrTAB T, long n) // fonction pour afficher le contenu de table
     }
 }
 
+ptr allouerptr() // cette fonction pour allouer un espace memoire pour un element de la liste chaine
+{
+    ptr L;
+
+    L = (ptr)malloc(sizeof(liste)); // allocation dynamique de pointeur vers un element de chaine de type liste
+    // L->entity_name = allouerstr();         // allouer la chaine de caractere dynamique
+    // L->entity_code = allouerstr();
+    // L->entity_type = allouerstr();
+    // L->constante = allouerstr();
+
+    L->entity_code[0] ='\0';
+    L->entity_code[0] ='\0';
+    L->entity_type[0] = '\0';
+    L->constante[0] = '\0';
+
+    L->tablenght=0 ;
+    L->svt1=NULL;
+    return (L);
+}
+
+char *allouerstr()
+{
+    char *ch;
+    ch = (char *)malloc(20 * sizeof(char)); 
+
+    if (ch == NULL)
+    {
+        printf("erreur de allocaion!!");
+        exit(-1);
+    }
+    return (ch);
+}
+
+ int main() {
+     int i,k;
+     char *ch;
+
+    // ch=allouerstr;
+
+     printf("saisir idf:");
+     scanf("%s", &ch);
+     k = hash_func(ch);
+
+     printf("\n\nhash code is : %d",k);
+
+     //  for(i=0;i<100;i++){
+     //      ts[i].svt2=NULL;
+     //  }
+
+     //  for(i=0;i<3;i++){
+     //      printf("saisir %d:",i+1);
+     //      printf("\n");
+     //      printf("saisir idf:");
+     //      scanf("%s",&ch);
+     //      k = hash_func(ch);
+     //  }
+
+     return 0;
+ }
