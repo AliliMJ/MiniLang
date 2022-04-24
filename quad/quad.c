eb035cb9f97d50397295cc4d4205f894029e7b8a
Alili <alili.mohamedlamine@gmail.com>
2022-04-25 00:08:57 +0100

    quadruplets initial

--- quad/quad.c
+++ quad/quad.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "Quad.h"
+#include "quad.h"
 int indq = 0;
 void quad(char *o, char *o1, char *o2, char *r)
 {
@@ -14,9 +14,13 @@ void quad(char *o, char *o1, char *o2, char *r)
 
 void afficherQuad()
 {
+  printf("\n\n");
+  printf("************************* Quadruplets **************************\n\n");
+
     int i;
     for (i = 0; i < indq; i++)
     {
         printf("%d-( %s , %s , %s , %s )\n", i, q[i].opr, q[i].op1, q[i].op2, q[i].res);
     }
+    printf("\n\n");
 }
--- unix.sh
+++ unix.sh
@@ -3,5 +3,5 @@ flex lexical.l
 cd ../syntax
 bison -d -v syntax.y
 cd ../
-gcc ./lexical/lex.yy.c ./syntax/syntax.tab.c ./tabsym/tabsym.c -ll -ly -o compiler.exe
+gcc ./lexical/lex.yy.c ./syntax/syntax.tab.c ./tabsym/tabsym.c ./quad/quad.c -ll -ly -o compiler.exe
 ./compiler.exe<code.txt
