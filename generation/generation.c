b59b3631674dca9f7be95b4344c7daa3baf95cda
Alili <alili.mohamedlamine@gmail.com>
2022-05-30 13:11:48 +0100

    generation

--- generation/generation.c
+++ generation/generation.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "generation.h"
-
+#include <math.h>
+int * br, nbrBranches;
 
 extern int indq;
 FILE *f;
@@ -14,7 +15,28 @@ FILE *f;
 // void codeSegment() {
 
 // }
+void branches() {
+  int b[indq], i, j=0, n;
+  for(i=0;i<indq;i++) {
+    if(q[i].opr!=NULL && q[i].opr[0]=='B') {
+      n = atoi(q[i].op1);
+      b[n]=1;
+      nbrBranches++;
+    }
+    else b[i] =0;
+  }
+  br = malloc(nbrBranches*sizeof(int));
+  j=0;
+  for(i=0;i<indq;i++) {
+    if(b[i]==1) {
+      br[j] = i;
+      j++;
+    }
+  }
 
+ 
+
+}
 void generateCode() {
 
   f = fopen("./generation/generation.txt", "w");
@@ -25,18 +47,72 @@ void generateCode() {
   }
 
   printf("text added !\n");
+  branches();
   int i;
-  //for(i=0;i< indq;i++) tranlate(quad[i]);
-  tranlate(q[8]);
+  printf(" %d ", br[0]);
+  int lastBranchIndex=0;
+  for(i=0;i< indq;i++) {
+    if(i==br[lastBranchIndex]) {
+      fprintf(f, "Etiq%d :", i);
+      lastBranchIndex++;
+    }
+    tranlate(q[i]);
+  }
+
   fclose(f);
 
 }
 
 void tranlate(quadruplet quad) {
-  if (strcmp(quad.opr, "=")==0) aff(quad.op1, quad.res);
+  if(quad.opr == NULL) return;
+ 
+  switch(quad.opr[0]) {
+    case '=':
+      aff(quad.op1, quad.res);
+
+      break;
+    case '+':
+      add(quad);
+      break;
+    case('*'):
+      mult(quad);
+      break;
+    case('-'):
+      minus(quad);
+      break;
+    case('/'):
+      div(quad);
+      break;
+    case 'B':
+      jmp(quad);
+      break;
+    default:
+
+    
+  }
 
 } 
 
 void aff(char* op, char* res) {
-  fprintf(f, "MOV %s %s", res, op);
+  if (isdigit(op[0])==0) {
+    fprintf(f, "MOV AX %s\n", op);
+    fprintf(f, "MOV %s AX\n", res);
+  }
+  else
+    fprintf(f, "MOV %s %s\n", res, op);
+}
+void jmp(quadruplet quad) {
+  
+}
+void minus(quadruplet quad) {
+  
+}
+void add(quadruplet quad) {
+  
+}
+void div(quadruplet quad) {
+  
+}
+void mult(quadruplet quad) {
+  
 }
--- generation/generation.h
+++ generation/generation.h
@@ -5,6 +5,11 @@
 void generateCode();
 void tranlate(quadruplet quad);
 void aff(char* op, char* res); 
+void jmp(quadruplet quad);
+void minus(quadruplet quad);
+void add(quadruplet quad);
+void div(quadruplet quad);
+void mult(quadruplet quad);
 
 
 
